138bbf6eb70cdc44d3dc6aa17eafcb11c56dee92
aamleshi <aamleshi@gmail.com>
2019-03-07 14:12:08 -0600

    BetterBatcherWorks

--- betterBatcher.py
+++ betterBatcher.py
@@ -2,7 +2,8 @@ import numpy as np
 from tqdm import tqdm
 import os
 import random
-
+from PIL import Image
+import pickle
 
 def aggregateFiles(classList, imgDir, shuffle= True):
     """
@@ -14,7 +15,8 @@ def aggregateFiles(classList, imgDir, shuffle= True):
     for classID in tqdm(classList):
         folderPath = imgDir + classID
         imagePathList = os.listdir(folderPath)
-        allImagePaths = allImagePaths + imagePathList
+        fullpath = [folderPath+ '/'+path for path in imagePathList]
+        allImagePaths = allImagePaths + fullpath
         allImageLabel = allImageLabel + [classID]*len(imagePathList)
     if shuffle:
         c = list(zip(allImagePaths, allImageLabel))
@@ -28,14 +30,15 @@ def train_validate_split(allImagePaths, allImageLabel, validfrac = .1):
     train_features = []
     train_labels = []
     assert(len(allImageLabel) == len(allImagePaths))
-    validIdx = np.ciel(len(allImagePaths)*validfrac)
+    validIdx = int(np.ceil(len(allImagePaths)*validfrac))
     valid_features = allImagePaths[:validIdx]
     valid_labels = allImageLabel[:validIdx]
-    valid_features = allImagePaths[validIdx:]
-    valid_labels = allImageLabel[validIdx:]
+    train_features = allImagePaths[validIdx:]
+    train_labels = allImageLabel[validIdx:]
     return (valid_features, valid_labels, train_features, train_labels)
 
 def one_hot_encode(x):
+    classes = ["Can", "Cookies", "Eggs", "Empty", "Fruit"]
     classEncoder = {classID:i for (i, classID) in enumerate(classes)}
     encoded = np.zeros((len(x), 5))
     for idx, val in enumerate(x):
@@ -50,37 +53,45 @@ def preprocess_and_save_data(imagePaths, labels, batchSize = 16):
     valid_features, valid_labels, train_features, train_labels = train_validate_split(imagePaths, labels)
     assert(len(valid_features) == len(valid_labels))
     assert(len(train_features) == len(train_labels))
+    print(len(valid_features))
     n_batches = len(train_features)//batchSize
 
-    batchPath = "batchedImages/"
+    batchPath = "batchedData/"
 
+    print("N_batches: ", n_batches)
     for batch_i in tqdm(range(1, n_batches + 1)):
         batchStart = (batch_i - 1) * batchSize
         batchEnd = batchStart + batchSize
-        features = train_features[batchStart:batchEnd]
-        labels = train_labels[batchStart:batchEnd]
+
+        featurePaths = train_features[batchStart:batchEnd]
         
-        # find index to be the point as validation data in the whole dataset of the batch (10%)
+        features = []
+        for path in tqdm(featurePaths):
+            pic = Image.open(path)
+            pix = np.array(pic.getdata()).reshape(pic.size[1], pic.size[0], 3)
+            features.append(pix)
+        print(len(features))
+        labels = train_labels[batchStart:batchEnd]
+        _preprocess_and_save(features, labels, batchPath+'preprocess_batch_' + str(batch_i) + '.p')
+    
+    print("saving validation")
 
-        # preprocess the 90% of the whole dataset of the batch
-        # - normalize the features
-        # - one_hot_encode the lables
-        # - save in a new file named, "preprocess_batch_" + batch_number
-        # - each file for each batch
-        _preprocess_and_save(features, labels, 
-                             batchPath+'preprocess_batch_' + str(batch_i) + '.p')
-    _preprocess_and_save(np.array(valid_features), np.array(valid_labels),
-                         batchPath+'preprocess_validation.p')
-    _preprocess_and_save(np.array(valid_features), np.array(valid_labels),
-                         batchPath+'preprocess_testing.p')
+    for path in train_features:
+            pic = Image.open(os.path.join(path,image))
+            pix = np.array(pic.getdata()).reshape(pic.size[1], pic.size[0], 3)
+            features.append(pix)
 
+    _preprocess_and_save(np.array(valid_features), np.array(valid_labels), batchPath+'preprocess_validation.p')
+    _preprocess_and_save(np.array(valid_features), np.array(valid_labels), batchPath+'preprocess_testing.p')
+    return n_batches
 
 def main():
     classList = ["Can", "Cookies", "Eggs", "Empty", "Fruit"]
     imgDir = "./Classes/"
     allImagePaths, allImageLabel = aggregateFiles(classList, imgDir)
     print("Images Found: ", len(allImageLabel))
-    print("TODO")
+    print(allImagePaths[1])
+    preprocess_and_save_data(allImagePaths, allImageLabel, batchSize = 16)
 
 if __name__ == "__main__":
     main()
