552a475b76f9f2e503a5a2ec3eb1a96e77f1c610
Arash <aamleshi@gmail.com>
2019-03-07 21:17:31 -0600

    got the smaller data things working so computer no feel bad:

--- betterBatcher.py
+++ betterBatcher.py
@@ -4,6 +4,9 @@ import os
 import random
 from PIL import Image
 import pickle
+from tempfile import TemporaryFile
+import skimage
+import skimage.transform
 
 def aggregateFiles(classList, imgDir, shuffle= True):
     """
@@ -45,9 +48,17 @@ def one_hot_encode(x):
         encoded[idx][classEncoder[val]] = 1       
     return encoded
 
-def _preprocess_and_save(features, labels, filename):
+def _preprocess_and_save(features, labels, folderpath):
     labels = one_hot_encode(labels)
-    pickle.dump((features, labels), open(filename, 'wb'))
+    try:
+        os.mkdir(folderpath)
+    except:
+        pass
+    with open(folderpath+"/labels", 'wb') as f:
+        pickle.dump( labels, f, protocol=2)
+    for i, feature in enumerate(features):
+        with open(folderpath+"/"+str(i),'wb') as f:
+            np.save(f, feature)
 
 def preprocess_and_save_data(imagePaths, labels, batchSize = 16):
     valid_features, valid_labels, train_features, train_labels = train_validate_split(imagePaths, labels)
@@ -59,7 +70,8 @@ def preprocess_and_save_data(imagePaths, labels, batchSize = 16):
     batchPath = "batchedData/"
 
     print("N_batches: ", n_batches)
-    for batch_i in tqdm(range(1, n_batches + 1)):
+    #for batch_i in tqdm(range(1, n_batches + 1)):
+    for batch_i in tqdm(range(1, 12)):
         batchStart = (batch_i - 1) * batchSize
         batchEnd = batchStart + batchSize
 
@@ -69,17 +81,23 @@ def preprocess_and_save_data(imagePaths, labels, batchSize = 16):
         for path in tqdm(featurePaths):
             pic = Image.open(path)
             pix = np.array(pic.getdata()).reshape(pic.size[1], pic.size[0], 3)
-            features.append(pix)
-        print(len(features))
+            feature = np.copy(pix).astype('float')
+            tmpFeature = skimage.transform.resize(feature, (224, 224))
+            tmpFeature = np.copy(tmpFeature).astype('uint8')        
+            features.append(tmpFeature)
         labels = train_labels[batchStart:batchEnd]
         _preprocess_and_save(features, labels, batchPath+'preprocess_batch_' + str(batch_i) + '.p')
     
     print("saving validation")
 
-    for path in train_features:
-            pic = Image.open(os.path.join(path,image))
+    features = []
+    for path in tqdm(valid_features):
+            pic = Image.open(path)
             pix = np.array(pic.getdata()).reshape(pic.size[1], pic.size[0], 3)
-            features.append(pix)
+            feature = np.copy(pix).astype('float')
+            tmpFeature = skimage.transform.resize(feature, (224, 224))
+            tmpFeature = np.copy(tmpFeature).astype('uint8')        
+            features.append(tmpFeature)
 
     _preprocess_and_save(np.array(valid_features), np.array(valid_labels), batchPath+'preprocess_validation.p')
     _preprocess_and_save(np.array(valid_features), np.array(valid_labels), batchPath+'preprocess_testing.p')
@@ -91,7 +109,7 @@ def main():
     allImagePaths, allImageLabel = aggregateFiles(classList, imgDir)
     print("Images Found: ", len(allImageLabel))
     print(allImagePaths[1])
-    preprocess_and_save_data(allImagePaths, allImageLabel, batchSize = 16)
+    preprocess_and_save_data(allImagePaths, allImageLabel)
 
 if __name__ == "__main__":
     main()
